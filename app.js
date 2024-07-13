8f7e8d3827152490f3c104d1d49d2649c6b01304
namansinghal <72150803+namansinghal111@users.noreply.github.com>
2024-07-13 15:36:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ mongoose
 
 //! Cors config
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: "https://mernexpensetrk.netlify.app",
 };
 app.use(cors(corsOptions));
 //!Middlewares
